686bbfad4cc13246c400a4a70325ccbfc2ea8d10
NMD-haru <111827134+NMD-haru@users.noreply.github.com>
2023-09-10 14:36:20 +0700

    router fix 1
    by ancer

--- testing/routes/users.routers.js
+++ testing/routes/users.routers.js
@@ -2,18 +2,21 @@
 //   authJwt
 // } = require("../middleware");
 const controller = require("../controllers/user.controller");
-module.exports = function (app) {
-  app.use(function (req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
- 
-  app.get("/api/getall", controller.findbyQuery);
-  app.get("/api/user/:id", controller.findOne);
-  app.put("/api/user/:id", controller.update);
-  app.post("/api/user", controller.create);
-  app.delete("/api/user/:id", controller.delete); 
-};
+const express = require("express");
+const router = express.Router();
+
+router.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, Content-Type, Accept"
+  );
+  next();
+});
+
+router.get("/api/getall", controller.findAll);
+router.get("/api/user/:id", controller.findOne);
+router.put("/api/user/:id", controller.update);
+router.post("/api/user", controller.create);
+router.delete("/api/user/:id", controller.delete);
+
+module.exports = router;
