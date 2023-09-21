30c61c04d1369a006229e11343d88700f93afb98
NMD-haru <111827134+NMD-haru@users.noreply.github.com>
2023-09-21 11:21:23 +0700

    9-21

--- testing/app.js
+++ testing/app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users.routers');
-
+var usersRouter = require('./routes/users.routes');
+var authsRouter=require('./routes/auth.routes');
 var app = express();
 
 // view engine setup
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/auth',authsRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
